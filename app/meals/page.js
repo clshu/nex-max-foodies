4fd5cbdf4d12a1abba6e0dfd28d3e03b5477cd9b
clshu <clshu2020@gmail.com>
2025-01-22 11:25:31 -0800

    Outputting Meals Data & Images With Unknown Dimensions

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,23 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from './meals-grid'
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <p>Here are all the meals</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delcious Meals, create{' '}
+          <span className={classes['highlight']}>your own</span>
+        </h1>
+        <p>Choose your favorite recipe and enjoy a delicious meal at home</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   )
 }
