463be58a7bf28edf6504465553e95f0053cc1b11
clshu <clshu2020@gmail.com>
2025-01-21 21:39:49 -0800

    Highlight link when it's selected.

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,10 +1,15 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
 import MainHeaderBackground from './main-header-background'
+import { usePathname } from 'next/navigation'
 
 export default function MainHeader() {
+  const path = usePathname()
+
   return (
     <>
       <MainHeaderBackground />
@@ -16,10 +21,22 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={
+                  path.startsWith('/meals') ? classes.active : undefined
+                }
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/comminity">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={path === '/community' ? classes.active : undefined}
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
